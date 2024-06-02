76b669a2bec2a498d05f411c0d9dd04894935382
ay7581239 <ay7581239@gmail.com>
2024-06-02 17:51:01 +0530

    route file

--- routes/url.js
+++ routes/url.js
@@ -1,1 +1,7 @@
-const mongoose=require('mongoose');
+const express=require("express"); 
+const {handleGenerateNewShortURL,handleGetAnalytics}=require('../controllers/url');
+const router=express.Router();
+
+router.post("/",handleGenerateNewShortURL);
+router.get('/analytics/:shortId',handleGetAnalytics);
+module.exports=router;
